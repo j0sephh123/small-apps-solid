577c95f2b1e3574bb2f835e1a69bd7f0084d7bd3
j0sephh123 <iossif123gg@gmail.com>
2023-01-01 21:15:39 +0200

    finish solid example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Component, createMemo, createSignal } from "solid-js";
 
 const App: Component = () => {
+  const [count, setCount] = createSignal(0);
+
+  const counterClassName = createMemo(() => {
+    if (count() > 0) {
+      return "success";
+    }
+
+    if (count() < 0) {
+      return "failure";
+    }
+  });
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div id="app">
+      <div class="content">
+        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+        <div class={counterClassName()} id="counter">
+          {count()}
+        </div>
+        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      </div>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 };
