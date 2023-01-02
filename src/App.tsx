ff18c57dad0c00ed9f17f31b73eb25c715bb0128
j0sephh123 <iossif123gg@gmail.com>
2023-01-02 10:29:55 +0200

    finish solid router

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
-import { Component, createMemo, createSignal } from "solid-js";
+import { Link, Route, Routes } from "@solidjs/router";
+import { Component } from "solid-js";
+import Counter from "./Counter";
 
 const App: Component = () => {
-  const [count, setCount] = createSignal(0);
-
-  const counterClassName = createMemo(() => {
-    if (count() > 0) {
-      return "success";
-    }
-
-    if (count() < 0) {
-      return "failure";
-    }
-  });
-
   return (
-    <div id="app">
-      <div class="content">
-        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
-        <div class={counterClassName()} id="counter">
-          {count()}
-        </div>
-        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-      </div>
-      <button onClick={() => setCount(0)}>Reset</button>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Link href="/">Home</Link>
+            <Link href="/counter">Counter</Link>
+          </>
+        }
+      />
+      <Route path="/counter" component={Counter} />
+    </Routes>
   );
 };
 
---
+++ src/Counter.tsx
@@ -0,0 +1,30 @@
+import { Component, createMemo, createSignal } from "solid-js";
+
+const Counter: Component = () => {
+  const [count, setCount] = createSignal(0);
+
+  const counterClassName = createMemo(() => {
+    if (count() > 0) {
+      return "success";
+    }
+
+    if (count() < 0) {
+      return "failure";
+    }
+  });
+
+  return (
+    <div id="app">
+      <div class="content">
+        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+        <div class={counterClassName()} id="counter">
+          {count()}
+        </div>
+        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      </div>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
